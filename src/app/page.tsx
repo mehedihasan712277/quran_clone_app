5cd046f4d726c450ee6459091a641e16032b02c9
mehedi <mehedihasan712277@gmail.com>
2026-05-09 08:06:17 +0600

    feat: add all styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 const page = () => {
-    return <div></div>;
+    return (
+        <div>
+            <p className="font-sans">Inter</p>
+            <p className="font-kfgqpc">بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ</p>
+            <p className="font-al-mushaf">بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ</p>
+            <p className="font-noorehuda">بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ</p>
+            <p className="font-calligraphy">بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ</p>
+            <p className="font-calligraphy">001</p>
+        </div>
+    );
 };
 
 export default page;
